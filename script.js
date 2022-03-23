5d7587372259dc7eff232b4f04729184277bdc60
Klara Suchanova <klara.suchanova@seznam.cz>
2022-03-23 20:49:43 +0100

    první commit mého portfolia

---
+++ script.js
@@ -0,0 +1,47 @@
+(function($){
+    $(function(){
+
+        /*Scroll to sections*/
+        $(".jq--scroll-skills").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--skills").offset().top}, 1000);
+        });
+
+        $(".jq--scroll-portfolio").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--portfolio").offset().top}, 1200);
+        });
+
+        $(".jq--scroll-customer-reviews").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--customer-reviews").offset().top}, 1400);
+        });
+
+        $(".jq--scroll-about-me").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--about-me").offset().top}, 1600);
+        });
+
+        $(".jq--scroll-contacts").click(function(){
+            $("html, body").animate({scrollTop: $(".jq--contacts").offset().top}, 1800);
+        });
+
+        /* Mobile navigation*/
+        $(".jq--nav-icon").click(function(){
+            $(".nav-background").fadeToggle();
+            $(".mobile-nav-back").fadeToggle();
+            $("nav ul").fadeToggle();
+        });
+
+
+        /* Change hamburger to cross vice versa*/
+
+        $(".jq--image-hamburger").click(function(){
+            if($(".jq--image-hamburger").attr("src") == "Images/hamburgerMenu.png")
+            {
+                $( $(".jq--image-hamburger").attr("src","Images/crossMenu.png"))    
+            }
+            else
+            {
+                $( $(".jq--image-hamburger").attr("src","Images/hamburgerMenu.png"))    
+            }
+        });
+
+    });
+})(jQuery);
